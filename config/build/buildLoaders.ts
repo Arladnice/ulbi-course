cf89f67a03e0b00f6502dc8e055e30ca918131f5
Denis Arlakov <denarlakov12@gmail.com>
2025-01-14 14:21:02 +0300

    feat: storybook

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import { buildCssLoader } from "./loaders/buildCssLoader";
 
 export function buildLaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
@@ -34,27 +34,7 @@ export function buildLaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     },
   };
 
-  const cssLoaders = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module")),
-            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
-          },
-        },
-      },
-      {
-        loader: "sass-loader",
-        options: {
-          api: "modern",
-        },
-      },
-    ],
-  };
+  const cssLoaders = buildCssLoader(isDev);
 
   return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoaders];
 }
